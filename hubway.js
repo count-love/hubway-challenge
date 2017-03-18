14f78cd366d8c86420bee07a9c4a6841251410cc
Tommy Leung <tommy@tommyleung.com>
2017-03-18 16:44:51 -0400

    Auto-color by cluster

--- hubway.js
+++ hubway.js
@@ -3,33 +3,34 @@ var activeMarkers = [];
 var map;
 var hubway = {data: []};
 
-var mapIconColors = {
-    'Boston': 'red', 
-    'Cambridge': 'blue',
-    'Somerville': 'green',
-    'default': 'white'
-};
-    
-var mapIcons = {};
-var mapIconsKMeans = [];
-Object.keys(mapIconColors).forEach(function(city) {
-    var newIcon = L.divIcon({ 
+var cssColors = ['black','silver','gray','white','maroon','red','purple','fuchsia','green','lime','olive','yellow','navy','blue','teal','aqua','antiquewhite','aquamarine','azure','beige','bisque','blanchedalmond','blueviolet','brown','burlywood','cadetblue','chartreuse','chocolate','coral','cornflowerblue','cornsilk','crimson','cyan','darkblue','darkcyan','darkgoldenrod','darkgray','darkgreen','darkgrey','darkkhaki','darkmagenta','darkolivegreen','darkorange','darkorchid','darkred','darksalmon','darkseagreen','darkslateblue','darkslategray','darkslategrey','darkturquoise','darkviolet','deeppink','deepskyblue','dimgray','dimgrey','dodgerblue','firebrick','floralwhite','forestgreen','gainsboro','ghostwhite','gold','goldenrod','greenyellow','grey','honeydew','hotpink','indianred','indigo','ivory','khaki','lavender','lavenderblush','lawngreen','lemonchiffon','lightblue','lightcoral','lightcyan','lightgoldenrodyellow','lightgray','lightgreen','lightgrey','lightpink','lightsalmon','lightseagreen','lightskyblue','lightslategray','lightslategrey','lightsteelblue','lightyellow','limegreen','linen','mediumaquamarine','mediumblue','mediumorchid','mediumpurple','mediumseagreen','mediumslateblue','mediumspringgreen','mediumturquoise','mediumvioletred','midnightblue','mintcream','mistyrose','moccasin','navajowhite','oldlace','olivedrab','orangered','orchid','palegoldenrod','palegreen','paleturquoise','palevioletred','papayawhip','peachpuff','peru','pink','plum','powderblue','rosybrown','royalblue','saddlebrown','salmon','sandybrown','seagreen','seashell','sienna','skyblue','slateblue','slategray','slategrey','snow','springgreen','steelblue','tan','thistle','tomato','turquoise','violet','wheat','whitesmoke','yellowgreen'];
+var mapMarkerColors = [];
+cssColors.forEach(function(color) {
+    mapMarkerColors.push(
+        L.divIcon({ 
             className: 'empty',
-            html: '<div class="marker" style="background:' + mapIconColors[city] + '"></div>'
-        });
-        
-    mapIcons[city] = newIcon;
-    mapIconsKMeans.push(newIcon);        
+            html: '<div class="marker" style="background:' + color + '"></div>'
+        })
+    );
 });
 
+var clusters = {};
+
+
 function addMarker(row) {
+   
+    // options: 'municipality', 'municipalityKMeans'
+    var clusterBy = 'municipalityKMeans';
    
     var latitude = row.latitude;
     var longitude = row.longitude;
     var description = row.station + ', ' + row['docksCount'] + ' bikes';
+
+    if (clusters[row[clusterBy]] === undefined) {
+        clusters[row[clusterBy]] = mapMarkerColors[Object.keys(clusters).length];
+    }
     
-    // var myIcon = mapIcons[row['municipality']] ? mapIcons[row['municipality']] : mapIcons['default'];
-    var myIcon = mapIconsKMeans[row['municipalityKMeans']];
+    var myIcon = clusters[row[clusterBy]];
     
     var marker = L.marker([latitude, longitude], {icon: myIcon}).addTo(map);
     marker.bindPopup(description);
@@ -74,8 +75,8 @@ function resizeMarkers() {
 function resetMapView() {
 	// contiguous US
 	map.fitBounds([
-		[42.254442496693386, -71.28787994384767],
-		[42.45740743905049, -70.90164184570314]
+		[42.33811807427539, -71.13733291625978],
+		[42.376934182549896, -71.00309371948244]
 	]);	
 }
 
